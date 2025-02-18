f8d0476504b3d7477b1dc30998239058a124ba19
marsya505 <marsyazulfazahrani0@gmail.com>
2025-02-18 15:32:46 +0700

    Percobaan 2

---
+++ MahasiswaMain11.java
@@ -0,0 +1,34 @@
+public class MahasiswaMain11 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+
+    public static void main(String[] args) {
+        MahasiswaMain11 mhs1 = new MahasiswaMain11();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
