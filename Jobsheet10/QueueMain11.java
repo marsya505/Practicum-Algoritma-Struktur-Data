bc65340fd73fe9f0a9fc7eb3965885a96f535824
marsya505 <marsyazulfazahrani0@gmail.com>
2025-05-06 13:41:32 +0700

    jobsheet 10

---
+++ Jobsheet10/QueueMain11.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class QueueMain11 {
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan:");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("-----------------");
+    }
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = input11.nextInt();
+        Queue11 Q = new Queue11(n);
+        int pilih;
+
+        do {
+            menu();
+            pilih = input11.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = input11.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 3 || pilih == 4 || pilih == 5);
+    }
+}
